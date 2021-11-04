9ca8734cb46cdd9b431276847c12361d6ffb49d2
MrRxdney (Rodney) <89966787+RodneyDaVinci@users.noreply.github.com>
2021-11-04 13:00:01 +0100

    Opdracht 10

---
+++ robotarm-python-2021-main/Opdracht10.py
@@ -0,0 +1,21 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 10')
+
+# Jouw python instructies zet je vanaf hier:
+
+for i in range(5):
+    robotArm.grab()
+
+    for a in range(9-(2*i)):
+        robotArm.moveRight()
+
+    robotArm.drop()
+
+    for b in range(9-(2*i)-1):
+        robotArm.moveLeft()
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
