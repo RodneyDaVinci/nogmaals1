423b8b4204dfc92cf26527d021308cecc30b98d2
MrRxdney (Rodney) <89966787+RodneyDaVinci@users.noreply.github.com>
2021-11-04 09:29:54 +0100

    Vijfde commit!

---
+++ duizelig/5.py
@@ -0,0 +1,6 @@
+enter= ''
+i = 0
+while enter != "stop":
+    enter = input('Voer hier een random getal/letter/woord/zin in: ')
+    i = i + 1
+    print ('Je hebt', i, 'keer de herhaalde input gebruikt.')
